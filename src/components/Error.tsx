9e3e11d048dcbadecba2c08c482eb37386dfba09
Paveltrety <pasha57@bk.ru>
2021-01-31 12:26:03 +0300

    Update Error.tsx

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div className='error'>
-       
+        <div className='error'>  
           <h3>Название города введено неверно. <br/>Попробуйте еще раз.</h3>
         </div>
     )
